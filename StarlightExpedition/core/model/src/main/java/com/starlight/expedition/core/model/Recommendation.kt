48654934e1ff7b1cd6e10763c6309ef70a2600c6
WOO8484 <qudrnr84@gmail.com>
2026-07-13 06:27:22 +0900

    Delete StarlightExpedition directory

--- StarlightExpedition/core/model/src/main/java/com/starlight/expedition/core/model/Recommendation.kt
+++
@@ -1,6 +0,0 @@
-package com.starlight.expedition.core.model
-
-data class Recommendation(
-    val game: Game,
-    val reason: RecommendationReason
-)
--- StarlightExpedition/feature/favorites/src/main/java/com/starlight/expedition/feature/favorites/FavoritesUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.starlight.expedition.feature.favorites
-
-import com.starlight.expedition.core.model.Game
-
-data class FavoritesUiState(
-    val loading: Boolean = true,
-    val favorites: List<Game> = emptyList(),
-    val errorMessage: String? = null
-)
