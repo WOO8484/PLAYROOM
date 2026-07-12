024405522e079e358b5a02f09ea7fa5d26ca713a
WOO8484 <qudrnr84@gmail.com>
2026-07-12 13:32:27 +0900

    Delete feature directory

--- feature/player/src/main/kotlin/com/playroom/feature/player/PlayerUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.playroom.feature.player
-
-import com.playroom.core.model.Game
-
-data class PlayerUiState(
-    val game: Game? = null,
-    val isPaused: Boolean = false,
-)
--- feature/search/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("playroom.android.feature")
-}
-
-android {
-    namespace = "com.playroom.feature.search"
-}
