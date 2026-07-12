56ded7288b3848de0d5580e3ae297554c02aca11
WOO8484 <qudrnr84@gmail.com>
2026-07-12 13:33:01 +0900

    Delete core directory

--- core/model/src/main/kotlin/com/playroom/core/model/GameSystem.kt
+++
@@ -1,9 +0,0 @@
-package com.playroom.core.model
-
-/** Retro console / system a [Game] belongs to. */
-enum class GameSystem(val label: String) {
-    GBA("GBA"),
-    SNES("SNES"),
-    PS1("PS1"),
-    MEGA_DRIVE("Mega Drive"),
-}
--- core/model/src/main/kotlin/com/playroom/core/model/LibraryFilter.kt
+++
@@ -1,8 +0,0 @@
-package com.playroom.core.model
-
-/** Filter chip selection on the 게임룸 (library) screen. */
-sealed interface LibraryFilter {
-    data object All : LibraryFilter
-    data object Favorites : LibraryFilter
-    data class BySystem(val system: GameSystem) : LibraryFilter
-}
