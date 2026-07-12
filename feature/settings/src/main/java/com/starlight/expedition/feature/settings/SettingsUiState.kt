2b70dc3a4402665776867d9c63b5e9506d1aa642
WOO8484 <qudrnr84@gmail.com>
2026-07-13 06:36:37 +0900

    Delete feature directory

--- feature/settings/src/main/java/com/starlight/expedition/feature/settings/SettingsUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.starlight.expedition.feature.settings
-
-data class SettingsUiState(
-    val darkModeEnabled: Boolean = false,
-    val soundEnabled: Boolean = true,
-    val autoSaveEnabled: Boolean = true
-)
